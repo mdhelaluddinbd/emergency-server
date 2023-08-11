cd4bb2a453d12cf8691ba89f206866dfdf3623a0
Md.Helal Uddin <mdhelaluddin.cse@gmail.com>
2023-08-11 17:31:00 +0600

    Update server site code

---
+++ config/main.config.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const uri =`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.ehwknzz.mongodb.net/emergency-servicedb?retryWrites=true&w=majority`;
+
+const databaseConnection = mongoose
+  .connect(uri, connectionParams)
+  .then(() => {
+    console.log("=========MongoDB Cloud is connected==========");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
